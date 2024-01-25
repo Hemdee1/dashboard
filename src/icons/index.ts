5eb738aa6918f916be9e0acd5873012a2f235325
hemdee <sanusi5110@gmail.com>
2024-01-25 14:11:58 +0100

    update icon

---
+++ src/icons/index.ts
@@ -0,0 +1,33 @@
+import ArrowRightIcon from "./arrowRight";
+import BellIcon from "./bell";
+import BoxIcon from "./box";
+import CalendarIcon from "./calendar";
+import CategoryIcon from "./category";
+import DiscountIcon from "./discount";
+import InfoIcon from "./info";
+import Logo from "./logo";
+import LogoutIcon from "./logout";
+import MoonIcon from "./moon";
+import ProfileIcon from "./profile";
+import SearchIcon from "./search";
+import SettingIcon from "./setting";
+import SunIcon from "./sun";
+import TrendUpIcon from "./trendUp";
+
+export {
+  CategoryIcon,
+  ArrowRightIcon,
+  BellIcon,
+  BoxIcon,
+  CalendarIcon,
+  DiscountIcon,
+  InfoIcon,
+  Logo,
+  LogoutIcon,
+  MoonIcon,
+  ProfileIcon,
+  SearchIcon,
+  SettingIcon,
+  SunIcon,
+  TrendUpIcon,
+};
